62a209d7295c180b96027352c158c26eed200e67
Arman Kolahan <arman.kolahan@tripix.tech>
2021-04-03 14:12:07 +0200

    navigation added

---
+++ app/src/main/java/com/armanco/integral/navigation/details/DetailsFragment.kt
@@ -0,0 +1,7 @@
+package com.armanco.integral.navigation.details
+
+import androidx.fragment.app.Fragment
+import com.armanco.integral.R
+
+class DetailsFragment: Fragment(R.layout.fragment_details) {
+}
---
+++ app/src/main/java/com/armanco/integral/navigation/list/ListFragment.kt
@@ -0,0 +1,7 @@
+package com.armanco.integral.navigation.list
+
+import androidx.fragment.app.Fragment
+import com.armanco.integral.R
+
+class ListFragment: Fragment(R.layout.fragment_list) {
+}
