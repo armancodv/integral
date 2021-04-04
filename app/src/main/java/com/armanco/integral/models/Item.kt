64b62e1aff0833d012f8085f911461f4b9b3555c
Arman Kolahan <arman.kolahan@tripix.tech>
2021-04-04 08:21:57 +0200

    index of category_id column

--- app/src/main/java/com/armanco/integral/models/Item.kt
+++ app/src/main/java/com/armanco/integral/models/Item.kt
@@ -7,4 +7,4 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class Item(@PrimaryKey val id: String, @ColumnInfo(name = "category_id") val categoryId: String, @ColumnInfo(name = "drawable_res") @DrawableRes val drawableRes: Int, @ColumnInfo(name = "string_res") @StringRes val stringRes: Int)
+data class Item(@PrimaryKey val id: String, @ColumnInfo(name = "category_id", index = true) val categoryId: String, @ColumnInfo(name = "drawable_res") @DrawableRes val drawableRes: Int, @ColumnInfo(name = "string_res") @StringRes val stringRes: Int)
