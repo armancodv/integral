5f330f054b3278c7a12f6a467ad69140206275e4
Arman Kolahan <arman.kolahan@tripix.tech>
2021-04-04 22:52:55 +0200

    admob added

--- app/src/main/java/com/armanco/integral/MainActivity.kt
+++ app/src/main/java/com/armanco/integral/MainActivity.kt
@@ -2,12 +2,23 @@ package com.armanco.integral
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initAdMob()
+    }
+
+    private fun initAdMob() {
+        MobileAds.initialize(this)
+        val adRequest = AdRequest.Builder().build()
+        adView?.loadAd(adRequest)
+
     }
 }
--- app/src/main/java/com/armanco/integral/navigation/image/ImageFragment.kt
+++ app/src/main/java/com/armanco/integral/navigation/image/ImageFragment.kt
@@ -4,14 +4,40 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.armanco.integral.R
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+import com.google.android.gms.ads.LoadAdError
 import kotlinx.android.synthetic.main.fragment_image.*
+
 
 class ImageFragment: Fragment(R.layout.fragment_image) {
 
+    private var mInterstitialAd: InterstitialAd? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        showInterstitialAd()
         arguments?.getInt(IMAGE_KEY)?.let { photoView?.setImageResource(it) }
     }
+
+    private fun showInterstitialAd() {
+        mInterstitialAd = InterstitialAd(activity)
+        mInterstitialAd?.adUnitId = "ca-app-pub-3940256099942544/1033173712"
+        val adRequest = AdRequest.Builder().build()
+        mInterstitialAd?.loadAd(adRequest)
+        mInterstitialAd?.adListener = object : AdListener() {
+            override fun onAdLoaded() {
+                mInterstitialAd?.show()
+            }
+            override fun onAdFailedToLoad(adError: LoadAdError) {}
+            override fun onAdOpened() {}
+            override fun onAdClicked() {}
+            override fun onAdLeftApplication() {}
+            override fun onAdClosed() {}
+        }
+    }
+
     companion object {
         const val IMAGE_KEY = "image"
     }
